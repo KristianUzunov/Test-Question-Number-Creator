d985f587c2584ab3de4f5cfb61ffe1e86d58ab9d
Kristian Uzunov <122904159+Krisblox@users.noreply.github.com>
2023-05-15 19:55:31 +0300

    Add files via upload
    This is the whole thing

---
+++ emptyQuestionSpaceMaker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace emptyQuestionSpaceMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number of questions on the test.");
+            string numberOfQuestions = Console.ReadLine();
+            bool isInt = true;
+            bool letPass = false;
+            int numberToTestCuzImTooStupidToThinkOfAnotherWayToDoThis = 0;
+            while (letPass == false) 
+            {
+                isInt = true;
+                foreach (var thing in numberOfQuestions)
+                {
+                    if (char.IsLetter(thing) || thing == '.')
+                    {
+                        isInt = false;
+                        Console.WriteLine("Please use a whole digit.");
+                        numberOfQuestions = Console.ReadLine();
+                        break;
+                    }
+                    if (isInt == true && numberToTestCuzImTooStupidToThinkOfAnotherWayToDoThis == numberOfQuestions.Length)
+                    {
+                       letPass = true;
+                    }
+                    numberToTestCuzImTooStupidToThinkOfAnotherWayToDoThis++;
+                }
+            }
+            for (int i = 1; i <= int.Parse(numberOfQuestions); i++)
+            {
+                Console.WriteLine($"{i} - ");
+            }
+        }
+    }
+}
